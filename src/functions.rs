b842bf75209f46a028ade67b555b9cf52258cd24
Nelly Njeri <nellycyberpro@gmail.com>
2024-07-04 13:26:37 +0300

    Deleted everything

--- src/functions.rs
+++
@@ -1,7 +0,0 @@
-// Defining a function
-fn greet() {
-    println!("Hello, Rust!");
-}
-fn add(a:i32, b:i32){
-    a+b
-}
--- src/shadowing.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let salary = 100.00;
-    let salary = 1.50 ; // reads first salary
-    println!("The value of salary is :{}",salary);
-    }
